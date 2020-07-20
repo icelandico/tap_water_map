53f277a803e0d3f640a4bf9b234c79730344cfc6
Michal Muszynski <m.muszy@gmail.com>
2020-07-20 19:29:45 +0200

    Add hover and blur functinoality

--- index.js
+++ index.js
@@ -19,7 +19,41 @@ const chooseColor = value => {
   if (value > 0) return "#d7191c";
 };
 
-const geojsonLayer = new L.GeoJSON.AJAX("countries.geojson", {style: countriesStyle});
+const highlightFeature = e => {
+  const layer = e.target;
+
+  layer.setStyle({
+    weight: 0.75,
+    color: '#1F2232',
+    dashArray: '',
+    fillOpacity: 0.7
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+    layer.bringToFront();
+  }
+}
+
+const resetHighlight = e => {
+  geojsonLayer.resetStyle(e.target);
+};
+
+const onEachFeature = (feature, layer) => {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: zoomToFeature
+  });
+}
+
+const geojsonLayer = new L.GeoJSON.AJAX("countries.geojson",{
+    style: countriesStyle,
+    onEachFeature: onEachFeature
+  });
+
+function zoomToFeature(e) {
+  mymap.fitBounds(e.target.getBounds());
+}
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/texastribune.map-3g2hqvcf/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
