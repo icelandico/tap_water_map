87271367808b77af10870034ffcb40990cdf378e
Michal Muszynski <m.muszy@gmail.com>
2020-08-12 20:53:32 +0200

    Reorganize code, move utility functions and constants

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const mymap = L.map('map-container').setView([51.505, -0.09], 2);
 const featureInfo = L.control();
 const legendElement = L.control({ position: 'bottomleft' });
-const URL_BASE = "https://www.canyoudrinktapwaterin.com/tap-water-safety-in";
 let currentFeature = "";
 let currentRating = "";
 let locked = false;
-
-const thresholds = [
-  {label: "< 20%", value: 19},
-  {label: "20%-39%", value: 39},
-  {label: "40%-59%", value: 59},
-  {label: "60%-79%", value: 79},
-  {label: "80%-100%", value: 100},
-];
-
-const chooseColor = value => {
-  if (!value) return "#ecf0f1";
-  if (value > 80) return "#406141";
-  if (value > 60) return "#08303b";
-  if (value > 40) return "#ff9f00";
-  if (value > 20) return "#ff5202";
-  if (value >= 0) return "#a70009";
-};
 
 featureInfo.onAdd = function () {
   this.div = L.DomUtil.create('div', 'info')
@@ -49,29 +31,6 @@ featureInfo.update = function (props) {
       `<a class="map__info--details-link" href="${URL_BASE}-${currentFeature.toLowerCase()}" >See details for ${currentFeature}</a>`
       ;
 };
-
-
-const countriesStyle = () => {
-  return {
-    stroke: true,
-    fill: true,
-    fillColor: '#325670',
-    color: '#7991a3',
-    weight: 0.3,
-    fillOpacity: 1
-  }
-};
-
-const markerHtmlStyles = value => `
-  background-color: ${chooseColor(value)};
-  min-width: 1.5rem;
-  min-height: 1.5rem;
-  display: block;
-  position: relative;
-  border-radius: 3rem 3rem 0;
-  transform: rotate(45deg);
-  border: 1px solid #FFFFFF
- `;
 
 const customIcon = feature => {
   const val = feature.properties.waterQuality;
@@ -186,7 +145,7 @@ const removeUsStates = () => {
   geojsonLayerCities.remove();
 };
 
-mymap.on('zoomend',function(e){
+mymap.on('zoomend',function(e) {
   const currentZoom = mymap.getZoom();
   if (currentZoom >= 5) {
     geojsonLayerCities.addTo(mymap)
---
+++ utils/index.js
@@ -0,0 +1,40 @@
+const URL_BASE = "https://www.canyoudrinktapwaterin.com/tap-water-safety-in";
+
+const thresholds = [
+  {label: "< 20%", value: 19},
+  {label: "20%-39%", value: 39},
+  {label: "40%-59%", value: 59},
+  {label: "60%-79%", value: 79},
+  {label: "80%-100%", value: 100},
+];
+
+const chooseColor = value => {
+  if (!value) return "#ecf0f1";
+  if (value > 80) return "#406141";
+  if (value > 60) return "#08303b";
+  if (value > 40) return "#ff9f00";
+  if (value > 20) return "#ff5202";
+  if (value >= 0) return "#a70009";
+};
+
+const countriesStyle = () => {
+  return {
+    stroke: true,
+    fill: true,
+    fillColor: '#325670',
+    color: '#7991a3',
+    weight: 0.3,
+    fillOpacity: 1
+  }
+};
+
+const markerHtmlStyles = value => `
+  background-color: ${chooseColor(value)};
+  min-width: 1.5rem;
+  min-height: 1.5rem;
+  display: block;
+  position: relative;
+  border-radius: 3rem 3rem 0;
+  transform: rotate(45deg);
+  border: 1px solid #FFFFFF
+ `;
